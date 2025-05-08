333973b4cd0d6761897d2a907ed590298ed1dfb7
Valentin Beshleaga <vbeshlyaga1@gmail.com>
2025-05-08 11:50:50 +0300

    polling

--- API/kittybot/kittybot.py
+++ API/kittybot/kittybot.py
@@ -1,9 +1,11 @@
-import telebot
+from telebot import TeleBot
 
-bot = telebot.TeleBot(token='8090276111:AAHuaYQUFDVstlIzRZDprilF_9uBfzLmMwo')
+bot = TeleBot(token='8090276111:AAHuaYQUFDVstlIzRZDprilF_9uBfzLmMwo')
 
 @bot.message_handler(content_types=['text'])
 def say_hi(message):
     chat = message.chat
     chat_id = chat.id
     bot.send_message(chat_id= chat_id, text='Привет, я KittyBot!')
+
+bot.polling()
