85583ca44011c012d9241652e910c76bd636a365
Valentin Beshleaga <vbeshlyaga1@gmail.com>
2025-05-08 11:54:59 +0300

    удачный эксперимент

--- API/kittybot/kittybot.py
+++ API/kittybot/kittybot.py
@@ -2,16 +2,17 @@ from telebot import TeleBot
 
 bot = TeleBot(token='8090276111:AAHuaYQUFDVstlIzRZDprilF_9uBfzLmMwo')
 
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    chat = message.chat
+    chat_id = chat.id
+    bot.send_message(chat_id= chat_id, text='Спасибо, что включили меня!')
+
 @bot.message_handler(content_types=['text'])
 def say_hi(message):
     chat = message.chat
     chat_id = chat.id
     bot.send_message(chat_id= chat_id, text='Привет, я KittyBot!')
 
-@bot.message_handler(commands=['start'])
-def wake_up(message):
-    chat = message.chat
-    chat_id = chat.id
-    bot.send_message(chat_id= chat_id, text='Спасибо, что включили меня!')
 
 bot.polling()
