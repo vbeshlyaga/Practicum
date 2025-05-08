f963ee8ede36bb7f9a865b9d9bc166c5fbb83fd3
Valentin Beshleaga <vbeshlyaga1@gmail.com>
2025-05-08 12:23:38 +0300

    проект KittyBot v1

--- API/kittybot/kittybot.py
+++ API/kittybot/kittybot.py
@@ -1,12 +1,27 @@
-from telebot import TeleBot
+import requests
+from telebot import TeleBot, types
 
 bot = TeleBot(token='8090276111:AAHuaYQUFDVstlIzRZDprilF_9uBfzLmMwo')
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message):
+    chat = message.chat
+    bot.send_photo(chat.id, get_new_image())
 
 @bot.message_handler(commands=['start'])
 def wake_up(message):
     chat = message.chat
-    name = chat.first_name
-    bot.send_message(chat_id = chat.id, text=f'Спасибо, что включили меня, {name}!')
+    name = message.chat.first_name
+
+    bot.send_message(chat_id=chat.id, text=f'Привет, {name}! Я пришлю тебе котика!')
+    bot.send_photo(chat.id, get_new_image())
 
 @bot.message_handler(content_types=['text'])
 def say_hi(message):
