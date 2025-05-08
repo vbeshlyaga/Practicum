a1ad0c258fd764b5fdf468c6c984f9396e88a2ef
Valentin Beshleaga <vbeshlyaga1@gmail.com>
2025-05-08 12:00:20 +0300

    обращение по имени

--- API/kittybot/kittybot.py
+++ API/kittybot/kittybot.py
@@ -5,8 +5,8 @@ bot = TeleBot(token='8090276111:AAHuaYQUFDVstlIzRZDprilF_9uBfzLmMwo')
 @bot.message_handler(commands=['start'])
 def wake_up(message):
     chat = message.chat
-    chat_id = chat.id
-    bot.send_message(chat_id= chat_id, text='Спасибо, что включили меня!')
+    name = chat.first_name
+    bot.send_message(chat_id = chat.id, text=f'Спасибо, что включили меня, {name}!')
 
 @bot.message_handler(content_types=['text'])
 def say_hi(message):
